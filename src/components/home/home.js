50e25043f361adb3b938adac2f7b919b862b7daf
Manu <manu.sebastian@wipro.com>
2020-10-13 21:05:06 +0530

    initial

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -4,56 +4,89 @@ import * as actionList from '../../actions/actions';
 import { connect } from 'react-redux';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 import Jumbotron from 'react-bootstrap/Jumbotron';
-class HomeComponent extends React.Component{
-    constructor(props){
+import { Spinner } from 'react-bootstrap';
+class HomeComponent extends React.Component {
+    constructor(props) {
         super(props);
+        this.state = {
+            loaded: false,
+            error: false
+        };
     }
-    componentDidMount(){
-            axios.get('https://raw.githubusercontent.com/danted4/profile/master/public/json/content.json')
-            .then(res=>{
-              this.props.updateAction(res.data.home);
+    componentDidMount() {
+        const img = new Image();
+        img.onload = () => {
+            this.setState({
+                loaded: true
+            });
+        };
+        img.onerror = () => {
+            this.setState({
+                error: true
+            });
+        };
+        img.src = process.env.PUBLIC_URL + "/images/prof2.jpg";
+        axios.get('https://raw.githubusercontent.com/danted4/profile/master/public/json/content.json')
+            .then(res => {
+                this.props.updateAction(res.data.home);
             })
             .catch(err => {
-              console.log(err);
+                console.log(err);
             })
     }
-    render(){
+    render() {
         console.log(this.props.appState.rootReducer)
         return <React.Fragment>
             <div id="home" className="row no-gutters">
                 <div className="col-xs-12 col-md-6 col-lg-6">
-                        <Jumbotron>
-                            <h1>
+                    <Jumbotron>
+                        <h1>
                             {this.props.appState.rootReducer.title}
-                            </h1>
-                            <p>
-                                {
-                                    this.props.appState.rootReducer.bio
-                                }
-                            </p>
-                            <p>
-                                <div className="row no-gutters">
-                                    <div className="col-xs-12 col-md-12 col-lg-12">
-                                        <span><strong>Phone</strong></span>&nbsp;:&nbsp;
+                        </h1>
+                        <p>
+                            {
+                                this.props.appState.rootReducer.bio
+                            }
+                        </p>
+                        <p>
+                            <div className="row no-gutters">
+                                <div className="col-xs-12 col-md-12 col-lg-12">
+                                    <span><strong>Phone</strong></span>&nbsp;:&nbsp;
                                         <CopyToClipboard text={this.props.appState.rootReducer.email} onCopy={() => alert('Contact Copied.')}>
                                         <span>{this.props.appState.rootReducer.phone}</span>
-                                        </CopyToClipboard>
-                                    </div>
-                                    <div className="col-xs-12 col-md-12 col-lg-12">
-                                        <span><strong>Email</strong></span>&nbsp;:&nbsp;
+                                    </CopyToClipboard>
+                                </div>
+                                <div className="col-xs-12 col-md-12 col-lg-12">
+                                    <span><strong>Email</strong></span>&nbsp;:&nbsp;
                                         <CopyToClipboard text={this.props.appState.rootReducer.email} onCopy={() => alert('Email Copied.')}>
                                         <span>{this.props.appState.rootReducer.email}</span></CopyToClipboard>
-                                    </div>
-                                    <div className="col-xs-12 col-md-12 col-lg-12">
-                                        <span><strong>Social</strong></span>&nbsp;:&nbsp;<span>icons</span>
-                                    </div>
                                 </div>
-                            </p>
-                        
-                        </Jumbotron>
+                                <div className="col-xs-12 col-md-12 col-lg-12">
+                                    <span><strong>Social</strong></span>&nbsp;:&nbsp;<span>icons</span>
+                                </div>
+                            </div>
+                        </p>
+
+                    </Jumbotron>
                 </div>
                 <div className="col-xs-12 col-md-6 col-lg-6">
-                    <img className="profimg" src={process.env.PUBLIC_URL + "/images/prof2.jpg"} alt="image" />
+                    {this.state.loaded ?
+                        <img className="profimg" src={process.env.PUBLIC_URL + "/images/prof2.jpg"} alt="image" />
+                        :
+                        <div className="row no-gutters" style={{ height: '100%' }}>
+                            <div className="col-xs-12 col-md-12 col-lg-12" style={{ height: '10%', top: '50%', transform: 'translateY(-50%)' }}>
+                                <Spinner animation="grow" variant="dark" role="status">
+                                    <span className="sr-only">Loading...</span>
+                                </Spinner>&nbsp;
+                                <Spinner animation="grow" variant="dark" role="status">
+                                    <span className="sr-only">Loading...</span>
+                                </Spinner>&nbsp;
+                                <Spinner animation="grow" variant="dark" role="status">
+                                    <span className="sr-only">Loading...</span>
+                                </Spinner>
+                            </div>
+                        </div>
+                    }
                 </div>
             </div>
         </React.Fragment>
@@ -61,16 +94,16 @@ class HomeComponent extends React.Component{
 }
 
 const mapStateToProps = state => {
-    return{
-      appState : state
+    return {
+        appState: state
     }
-  
-  }
-  const mapDispatchToProps = dispatch =>{
-    return{
-      updateAction : (data) => dispatch(actionList.updateStateActionCreator(data)),
-      logoutAction : () => dispatch({})
+
+}
+const mapDispatchToProps = dispatch => {
+    return {
+        updateAction: (data) => dispatch(actionList.updateStateActionCreator(data)),
+        logoutAction: () => dispatch({})
     }
-  }
-  
-  export default connect(mapStateToProps, mapDispatchToProps)(HomeComponent);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(HomeComponent);
