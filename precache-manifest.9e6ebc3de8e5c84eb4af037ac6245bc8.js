4052161a424b7d7afa8d859cca2970ccfb15af3e
Manu <manu.sebastian@wipro.com>
2020-10-13 15:33:23 +0530

    Updates

---
+++ precache-manifest.9e6ebc3de8e5c84eb4af037ac6245bc8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b39504786a4224d8750d5ceacfe14064",
+    "url": "/profile/index.html"
+  },
+  {
+    "revision": "1bec5af0f977d68546c9",
+    "url": "/profile/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "cfcef9ce368d7021fba0",
+    "url": "/profile/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1bec5af0f977d68546c9",
+    "url": "/profile/static/js/2.f22cb9cb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/profile/static/js/2.f22cb9cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfcef9ce368d7021fba0",
+    "url": "/profile/static/js/main.1a6eabd9.chunk.js"
+  },
+  {
+    "revision": "1c45b0c2c616b3bc039e",
+    "url": "/profile/static/js/runtime-main.db6974f0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/profile/static/media/logo.5d5d9eef.svg"
+  }
+]);
